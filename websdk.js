39b81ab358475b22e10cce81c6c74a544c62a83d
Vitaly Sergey <vitaly.sergey@webpay.by>
2023-03-20 15:24:48 +0300

    example

---
+++ websdk.js
@@ -0,0 +1,37 @@
+class FacePassWebSDK {
+    constructor(token, webSdkContainer) {
+        this.token = token;
+        this.webSdkContainer = webSdkContainer;
+    }
+
+    init() {
+        fetch(`/enrollment/web-sdk`, {
+            headers: new Headers({
+                'Authorization': 'Bearer ' + this.token,
+                'Content-Type': 'application/json'
+            })
+        })
+            .then((response) => {
+                return new Promise((resolve) => response.json()
+                    .then((json) => resolve({
+                        status: response.status,
+                        ok: response.ok,
+                        json
+                    })));
+            })
+            .then(({status, json, ok}) => {
+                    ok ? this.openIframe(json.iframeUrl) : this.webSdkContainer.append(json.message)
+                }
+            )
+    }
+
+    openIframe(iframeUrl) {
+        const iframe = document.createElement('iframe');
+        iframe.setAttribute("src", iframeUrl);
+        iframe.style.position = "fixed";
+        iframe.style.border = "none";
+        iframe.style.height = "80vh";
+        iframe.style.width = "100vw";
+        this.webSdkContainer.appendChild(iframe);
+    }
+}
